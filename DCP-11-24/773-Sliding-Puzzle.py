4dff24d4ef5aad8245daca18f8af768ca24c48c3
Mario Nageh <marionageh7@gmail.com>
2024-11-25 22:26:10 +0200

    [773] [Time Beats: 97.35%] [Memory Beats: 79.25%]

---
+++ DCP-11-24/773-Sliding-Puzzle.py
@@ -0,0 +1,29 @@
+class Solution:
+    def slidingPuzzle(self, board: List[List[int]]) -> int:
+        adj = {
+            0: [1,3],
+            1: [0,2,4],
+            2: [1,5],
+            3: [0,4],
+            4: [1,3,5],
+            5: [2,4]
+        }
+        b = ''.join([str(c) for row in board for c in row])
+        q = deque([(b.index("0"),b,0)])
+        visit = set([b])
+        while q:
+            i,b,l = q.popleft()
+
+            if b == "123450":
+                return l
+
+            b_arr = list(b)
+            for j in adj[i]:
+                new_b_arr = b_arr[:]
+                new_b_arr[i],new_b_arr[j] = b_arr[j],b_arr[i]
+                new_b = "".join(new_b_arr)
+
+                if new_b not in visit:
+                    q.append((j,new_b,l + 1))
+                    visit.add(new_b)
+        return -1
